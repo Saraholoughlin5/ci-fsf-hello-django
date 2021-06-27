a8a6cf85e2b7273bfc0f99efac169309cd623e3b
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-06-27 17:28:33 +0000

    Fix alllowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3%ap1n(_7^g-j9lxh*j#rx!&0l$yuosk9neoa#vy2tnix98bb+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sol5-django-todo-app.herokuapp.com']
 
 
 # Application definition
